59bd474218dd8c1ed9743cac5362ec32c1586982
WSL <2112873995@qq.com>
2026-01-05 23:13:34 +0800

    上传了文章相关的proto文件，并且生成了相关服务代码

---
+++ service/article/rpc/article.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"sea-try-go/service/article/rpc/internal/config"
+	"sea-try-go/service/article/rpc/internal/server"
+	"sea-try-go/service/article/rpc/internal/svc"
+	"sea-try-go/service/article/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/article.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		__.RegisterArticleServiceServer(grpcServer, server.NewArticleServiceServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
