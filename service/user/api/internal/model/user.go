171c9a89dfefb910620aafb1f390e30124add607
unicornshjl <3159669749@qq.com>
2026-01-12 18:59:49 +0800

    feat(user,admin):给user的信息类型添加了status变量,表示用户状态

--- service/user/api/internal/model/user.go
+++ service/user/api/internal/model/user.go
@@ -7,6 +7,7 @@ type User struct {
 	Username   string            `gorm:"column:username"`
 	Password   string            `gorm:"column:password"`
 	Email      string            `gorm:"column:email"`
+	Status     int64             `gorm:"column:status;default:0"`
 	ExtraInfo  map[string]string `gorm:"column:extra_info;serializer:json"`
 	CreateTime time.Time         `gorm:"column:create_time;autoCreateTime"`
 	UpdateTime time.Time         `gorm:"column:update_time;autoUpdateTime"`
