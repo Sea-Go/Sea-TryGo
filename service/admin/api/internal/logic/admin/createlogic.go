ace1c07b6aa797c6eab5bb6014b11e09fbff3915
unicornshjl <3159669749@qq.com>
2026-01-15 16:41:12 +0800

    feat(admin):添加创建管理员逻辑

--- service/admin/api/internal/logic/admin/createlogic.go
+++ service/admin/api/internal/logic/admin/createlogic.go
@@ -6,10 +6,9 @@ package admin
 import (
 	"context"
 
-	"sea-try-go/service/admin/api/internal/model"
 	"sea-try-go/service/admin/api/internal/svc"
 	"sea-try-go/service/admin/api/internal/types"
-	"sea-try-go/service/common/cryptx"
+	"sea-try-go/service/admin/rpc/pb"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -30,21 +29,19 @@ func NewCreateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateLogi
 
 func (l *CreateLogic) Create(req *types.CreateAdminReq) (resp *types.CreateAdminResp, err error) {
 
-	password, e := cryptx.PasswordEncrypt(req.Password)
-	if e != nil {
-		return nil, e
-	}
-	admin := model.Admin{
+	rpcReq := &pb.CreateAdminReq{
 		Username:  req.Username,
-		Password:  password,
+		Password:  req.Password,
 		Email:     req.Email,
 		ExtraInfo: req.Extrainfo,
 	}
-	err = l.svcCtx.DB.Save(&admin).Error
+
+	rpcResp, err := l.svcCtx.AdminRpc.CreateAdmin(l.ctx, rpcReq)
 	if err != nil {
 		return nil, err
 	}
+
 	return &types.CreateAdminResp{
-		Id: admin.Id,
+		Id: rpcResp.Id,
 	}, nil
 }
